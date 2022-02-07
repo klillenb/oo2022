cdc15fb1424733440c4a36f91d3ea52b405b839d
klillenb <kertlil@greeny.cs.tlu.ee>
2022-02-07 19:15:55 +0200

    Tunnitöö tehtud

--- 01_funktsioon/StandardDeviation.java
+++ 01_funktsioon/StandardDeviation.java
@@ -1,24 +1,43 @@
 public class StandardDeviation{
-    //leian keskmise
-    public static double average(String[] sd){
+
+    public static double[] toNumbers(String[] sd){ //double[] on massiiv
+        double[] nums = new double[sd.length];
+        for(int i = 0; i < sd.length; i++){
+            nums[i] = Double.parseDouble(sd[i]);
+        }
+        return nums;
+    }
+
+    //leian keskmise ja hälbe
+    public static double[] deviation(double[] sd){
         double sum = 0;
+        double mean = 0;
         for(int i = 0; i < sd.length; i++){
             sum += Double.valueOf(sd[i]);
         }
-        System.out.println(sum);
-        return sum / sd.length;
-    }
-
-    //leian hälbe
-    public static double[] deviation(String[] sd){
+        mean = sum / sd.length;
         double[] v = new double[sd.length];
         for(int i = 0; i < sd.length; i++){
-            v[i] = 
+            v[i] = Math.pow((Double.valueOf(sd[i]) - mean), 2);
         }
+        return v;
+    }
+
+    public static double stdev(double[] nums){
+        double sum = 0;
+        double average = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += Double.valueOf(nums[i]);
+        }
+        average = sum / nums.length;
+        double stdev = Math.sqrt(average);
+        return stdev;
     }
 
     public static void main(String[] args){
-        System.out.println(average(args));
+        double[] numbers = toNumbers(args);
+        double[] nums = deviation(numbers);
+        System.out.println(stdev(nums));
     }
 
 
