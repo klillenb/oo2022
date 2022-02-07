5c795b0d137619de91c3c2deb5fc4ecf9f21ce64
klillenb <kertlil@greeny.cs.tlu.ee>
2022-02-07 15:08:38 +0200

    07.02

--- 01_funktsioon/Funktsioon1.java
+++ 01_funktsioon/Funktsioon1.java
@@ -6,10 +6,23 @@ public class Funktsioon1{
     public static double tollideks(double sentimeetrid){
         return sentimeetrid / 2.54;
     }
+    public static double[] reaalarvudeks(String[] sd){ //double[] on massiiv
+        double[] v = new double[sd.length];
+        for(int i = 0; i < sd.length; i++){
+            v[i] = Double.parseDouble(sd[i]);
+        }
+        return v;
+    }
     public static void main(String[] args){
-        if(args.length > 0){
-            double t = Double.parseDouble(args[0]);
-            System.out.println(tollideks(t));
-        } 
+        double[] arvud = reaalarvudeks(args);
+        for(int i = 0; i < arvud.length; i++){
+            System.out.println(sentimeetriteks(arvud[i]));
+        }
     }
 }
+/* 
+[kertlil@greeny 01_funktsioon]$ java Funktsioon1 2 8 16.3
+5.08
+20.32
+41.402
+*/
