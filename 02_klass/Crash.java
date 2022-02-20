afc1676ca18483f6999ea9023ef97bea32405f36
klillenb <kertlil@greeny.cs.tlu.ee>
2022-02-21 01:48:23 +0200

    broken

---
+++ 02_klass/Crash.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Crash{
+    public static void main(String[] arg){
+        int[][] table = new int[10][10];
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Sisesta esimese teekond: ");
+        String t1Instruction = reader.nextLine();
+
+        System.out.print("Sisesta teise teekond: ");
+        String t2Instruction = reader.nextLine();
+
+        Turtle t1 = new Turtle();
+        Turtle t2 = new Turtle();
+
+        for(int i = 0; i < t1Instruction.length(); i++){
+            if(t1Instruction.charAt(i) == 'f'){
+                t1.forward(); 
+                table[t1.askY()][t1.askX()] = 1;
+            } 
+            if(t1Instruction.charAt(i) == 't'){
+                t1.turn(); 
+                table[t1.askY()][t1.askX()] = 1;
+            }
+            System.out.println(t1);
+        } 
+
+        for(int j = 0; j < t2Instruction.length(); j++){
+            if(t2Instruction.charAt(j) == 'f'){
+                t2.forward();
+                table[t2.askX()][t2.askY()] = 1;
+            }
+            if(t2Instruction.charAt(j) == 't'){
+                t2.turn();
+                table[t2.askX()][t2.askY()] = 1;
+            }
+            //System.out.printl(t2);
+        }
+
+        for(int k = 0; k < 10; k++){
+            for(int l = 0; l < 10; l++){
+                System.out.print(table[k][l]);
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
